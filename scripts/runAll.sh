cc14c8a70990a8f74d52da90d0f48d0053cf22d3
Martin Vala <Martin.Vala@cern.ch>
2012-09-09 11:57:55 +0200

    Now macro is copied to run directoru

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -9,8 +9,11 @@ export LD_LIBRARY_PATH="$PROJECT_DIR/lib:$LD_LIBRARY_PATH"
 
 MY_PWD=`pwd`
 MY_RUN_DIR=$PROJECT_DIR/run/${1/.C/}
+rm -Rf $MY_RUN_DIR
 mkdir -p $MY_RUN_DIR
 
 cd $MY_RUN_DIR
-root -l $PROJECT_DIR/macros/MainRun.C'("'$PROJECT_DIR/$1'","'$PROJECT_DIR/'")'
+cp $PROJECT_DIR/$1 .
+MACRO=$(basename $1)
+root -l $PROJECT_DIR/macros/MainRun.C'("'$MACRO'","'$PROJECT_DIR/'")'
 cd $MY_PWD
